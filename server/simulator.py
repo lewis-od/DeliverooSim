58307ed97652d097a365a2307fc77445201ae1cc
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2018-11-24 14:14:03 +0000

    Add restaurant endpoint

--- server/simulator.py
+++ server/simulator.py
@@ -1,8 +1,25 @@
 from flask import Blueprint, g, request, jsonify
 from server.config import api_key
+import googlemaps
+import random
 
 bp = Blueprint('simulator', __name__)
 
 @bp.route('/')
 def index():
-    return api_key
+    return "Hello, world"
+
+@bp.route('/restaurant/<string:location>')
+def restaurant(location):
+    maps = googlemaps.Client(key=api_key)
+    response = maps.places('restaurant', location=location)
+    if response['status'] != "OK":
+        raise Exception("Google maps error")
+
+    results = response['results']
+    restaurant = random.choice(results)
+    res_dict = {
+        'location': restaurant['geometry']['location'],
+        'name': restaurant['name']
+    }
+    return jsonify(res_dict)
